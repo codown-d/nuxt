ceabd4bf3a6078a902445f8b5e4191e8ae210d00
张和 <zhanghe0@126.com>
2018-12-03 17:57:53 +0800

    first commit

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ async function start() {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port)
+  app.listen(port,host)
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true
